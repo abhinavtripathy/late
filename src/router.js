b9d5be7a5bb4c821dfa90787b478288eb91b8e7f
Frank Matranga <thefrankmatranga@gmail.com>
2019-11-09 10:39:24 -0500

    Start to document router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,31 @@
+/**
+ * This file declares ALL of LATE's frontend routes. This is every page that users see
+ * and navigate to. We use vue-router for this. Documentation can be found at https://router.vuejs.org/
+ *
+ * Our router is in 'history' mode which means that it simulates how traditional web apps work
+ * with changing urls even though we never actually request new pages.
+ *
+ * Every route is simply an object with the following schema (at minimum):
+ * {
+ *    path: '/path',
+ *    name: string,
+ *    component: () => import('path to page component),
+ *    meta: {
+ *      title: 'Page Title'
+ *    }
+ * }
+ *
+ * Some routes also have child routes. Check the documentation or just look
+ * at the many examples below to see how those work!
+ */
+
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// Gives us access to Axios (you might know it as $http in the components) to make API calls
 import api from './api'
 
+// Gives us access to Vuex
 import store from '@/store'
 
 import { ToastProgrammatic as Toast, SnackbarProgrammatic as Snackbar } from 'buefy'
@@ -11,9 +34,13 @@ Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.BASE_URL, // e.g. http://localhost:3000 or https://late.work
   linkActiveClass: 'is-active',
+  /**
+   * If on mobile, scroll down to the content of the page which is in a div with id #content
+   */
   scrollBehavior (to, from, savedPosition) {
+    // Fallible mobile checking
     if (
       typeof window.orientation !== 'undefined' ||
       navigator.userAgent.indexOf('IEMobile') !== -1
@@ -426,10 +453,21 @@ const router = new Router({
   ]
 })
 
+/**
+ * This is run before a route is entered. We mainly use this to grab all the important
+ * data about the user and their documents at page load (before the very first route finishes).
+ * This is definitely not the best way to do this...
+ */
 router.beforeEach(async (to, from, next) => {
+  // Close the course modal if it was open and a link in it was clicked
   if (store.state.courseModal.open) store.commit('CLOSE_COURSE_MODAL')
-  if (store.state.navbarExpanded) store.commit('TOGGLE_NAVBAR') // Close the navbar if clicked on
 
+  // Close the navbar if clicked on
+  if (store.state.navbarExpanded) store.commit('TOGGLE_NAVBAR')
+
+  // If in development mode, before a route completes prompt the user (developer)
+  // for a RCS ID of the user to log in as. If it is left blank then we do not
+  // authenticate and the developer can browse around not logged in.
   if (
     process.env.NODE_ENV === 'development' &&
     store.state.auth.isAuthenticated === null
@@ -447,6 +485,11 @@ router.beforeEach(async (to, from, next) => {
     await store.dispatch('GET_USER')
   }
 
+  // If the page is first loading and the user is logged in,
+  // make all the required API calls to get the data that
+  // Vuex holds: terms, the user's current courses, the user's
+  // current unavailabilities, the user's UPCOMING assessments,
+  // the user's todos, and all announceents.
   if (store.state.auth.isAuthenticated && !store.state.loaded) {
     await store.dispatch('GET_TERMS')
     const calls = []
@@ -460,13 +503,18 @@ router.beforeEach(async (to, from, next) => {
     calls.concat([
       store.dispatch('GET_TODOS'),
       store.dispatch('GET_ANNOUNCEMENTS'),
-      store.dispatch('AUTO_UPDATE_NOW')
+      store.dispatch('AUTO_UPDATE_NOW') // Start auto-updating the time every minute so LATE always knows when it is
     ])
-    await Promise.all(calls)
-    store.commit('SET_LOADED', true)
+    await Promise.all(calls) // Run all these in parallel to be quicker!
+    store.commit('SET_LOADED', true) // *Nice* All Loaded
   }
 
+  // If a route has a specific page title on it (meta.title) then apply it!
+  // The only issue with this is that routes missing `meta.title` will be
+  // navigated to but the page title will remain from whatever page previously
+  // visited! Therefore all routes should have titles or set them in their components.
   if (to.meta.title) document.title = to.meta.title + ' | LATE'
+
   if (
     to.matched.some(record => record.meta.requiresAuth) &&
     !store.state.auth.isAuthenticated
@@ -477,6 +525,9 @@ router.beforeEach(async (to, from, next) => {
     return
   }
 
+  // Prevent non-admins from navigating to admin-only pages!
+  // Even if they make it their, the API routes won't go through
+  // unless they are actual admins.
   if (
     to.matched.some(record => record.meta.requiresAdmin) &&
     !store.state.auth.user.admin
@@ -486,9 +537,11 @@ router.beforeEach(async (to, from, next) => {
       type: 'is-warning',
       duration: 3000
     })
-    return next('/')
+    return next('/') // Redirect them home with a slap on the wrist
   }
 
+  // If the user is logged in and is also on break, prevent their
+  // access to any school-related pages (defined in each route's meta field).
   if (
     to.matched.some(record => record.meta.cantViewOnBreak) &&
     store.getters.onBreak &&
@@ -504,9 +557,10 @@ router.beforeEach(async (to, from, next) => {
         router.push({ name: 'setup-terms' })
       }
     })
-    return next('/')
+    return next('/') // Redirect them home (the path home is '/') with the Snackbar message
   }
 
+  // Continue on to the actual route!
   next()
 })
 
